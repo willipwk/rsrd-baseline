474053aa6a5bba6eea395359fca49a5bfd9369d9
willipwk <p_w_kun@163.com>
2025-04-09 09:51:16 -0700

    update experiment scripts

--- reconstruction.py
+++ reconstruction.py
@@ -6,14 +6,38 @@ import glob
 with open("partnet_mobility_data_split.yaml", "r") as f:
     data_config = yaml.safe_load(f)
 test_video_list = data_config["test"]
+# test_video_list = ["sim_data/partnet_mobility/Scissors/10973/joint_0_bg/view_1/",
+#                    "sim_data/partnet_mobility/Refrigerator/10867/joint_0_bg/view_1/",
+#                    "sim_data/partnet_mobility/USB/100072/joint_0_bg/view_0/",]
+# obj_id_list = ["10867", "10973", "100072"]
+obj_id_list = []
 for test_video_path in test_video_list:
     # may need to modify the scan data path
-    test_scan_path = test_video_path[:test_video_path.rfind("view_")] + "view_init_rsrd/"
-    os.system(f"ns-train garfield --data {test_scan_path}")
+    obj_id = test_video_path.split('/')[3]
+    if obj_id in obj_id_list:
+        continue
+    obj_id_list.append(obj_id)
+    test_scan_path = "data/" + test_video_path[test_video_path.find("partnet"):test_video_path.rfind("view_")] + "view_init_rsrd/"
+    outdir = f"outputs/" + test_video_path[test_video_path.find("partnet"):test_video_path.rfind("view_")]
+    garfield_path = f"{outdir}/view_init_rsrd/garfield/"
+    if not os.path.exists(garfield_path):
+        os.system(f"ns-train garfield --data {test_scan_path} --output-dir {outdir} --experiment-name view_init_rsrd --viewer.quit-on-train-completion True")
 
-    outdir = f"outputs/{test_scan_path}"
-    garfield_path = f"{outdir}/garfield/"
     garfield_config_path_list = glob.glob(f"{garfield_path}/*/config.yml")
-    assert len(garfield_config_path_list) > 0, "no garfield config found"
+    if len(garfield_config_path_list) == 0:
+        continue
     garfield_config_path = garfield_config_path_list[-1]
-    os.system(f"ns-train dig --data {test_scan_path} --pipeline.garfield-ckpt {garfield_config_path}")
+    dig_path = f"{outdir}/view_init_rsrd/dig/"
+    if not os.path.exists(dig_path):
+        os.system(f"ns-train dig --data {test_scan_path} --pipeline.garfield-ckpt {garfield_config_path} --output-dir {outdir} --experiment-name view_init_rsrd \
+                --viewer.quit-on-train-completion True")
+    
+    # if os.path.exists(f"{outdir}/view_init_rsrd/state.pt"):
+    #     continue
+    # print(dig_path)
+    # dig_config_path_list = glob.glob(f"{dig_path}/*/config.yml")
+    # dig_config_path_list.sort()
+    # if len(dig_config_path_list) == 0:
+    #     continue
+    # dig_config_path = dig_config_path_list[-1]
+    # os.system(f"ns-viewer --load-config {dig_config_path} --viewer.websocket-host 127.0.0.1")
--- tracking.py
+++ tracking.py
@@ -7,35 +7,39 @@ import glob
 with open("partnet_mobility_data_split.yaml", "r") as f:
     data_config = yaml.safe_load(f)
 test_video_list = data_config["test"]
-dataset_config_path = "path/to/dataset/config/file"
+dataset_config_path = "new_partnet_mobility_dataset_correct_intr_meta.json"
 with open(dataset_config_path, 'r') as f_dataset:
     dataset_config_dict = json.load(f_dataset)
-
+# test_video_list = ["sim_data/partnet_mobility/Refrigerator/10867/joint_0_bg/view_1/"]
 for test_video_path in test_video_list:
     # may need to modify the test video path
-    cat = test_video_path.split('/')[2]
-    obj_id = test_video_path.split('/')[3]
-    joint = test_video_path.split('/')[4]
-    joint_id = int(joint[5:6])
+    test_video_path = test_video_path[test_video_path.find("partnet"):]
+    cat = test_video_path.split('/')[1]
+    obj_id = test_video_path.split('/')[2]
+    joint = test_video_path.split('/')[3]
+    joint_id = int(joint[6:7])
     joint_type = None
-    interaction_list = dataset_config_dict[cat][obj_id]["interaction"]
+    interaction_list = dataset_config_dict[cat][obj_id]["interaction_list"]
     for interaction in interaction_list:
         if joint_id == interaction["id"]:
             joint_type = interaction["type"]
     assert joint_type is not None, "cannot find joint in dataset config"
 
     test_scan_path = test_video_path[:test_video_path.rfind("view_")] + "view_init_rsrd/"
+    view_name = test_video_path[test_video_path.rfind("view_"):]
     outdir = f"outputs/{test_scan_path}"
     dig_path = f"{outdir}/dig/"
     dig_config_path_list = glob.glob(f"{dig_path}/*/config.yml")
     assert len(dig_config_path_list) > 0, "no dig config found"
+    dig_config_path_list.sort()
     dig_config_path = dig_config_path_list[-1]
 
     is_obj_jointed = "True" if joint_type == "hinge" else "False"
 
-    os.system(f"python scripts/run_tracker.py --is-obj-jointed {is_obj_jointed} \
-                --dig-config-path {dig_config_path} \
-                --video-path {test_video_path}/video.mp4 \
-                --output-dir {outdir} \
-                --no-save-hand")
+    if not os.path.exists(f"{outdir}/{view_name}"):
+        os.system(f"python scripts/run_tracker.py --is-obj-jointed {is_obj_jointed} \
+                    --dig-config-path {dig_config_path} \
+                    --video-path data/{test_video_path}/video.mp4 \
+                    --output-dir {outdir}/{view_name} \
+                    --no-save-hand")
     
